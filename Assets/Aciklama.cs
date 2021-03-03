52ec5695596429c5f2427581344ceb6d4c38f2c7
Cansu Yagsiz <73892079+cansuyagsiz@users.noreply.github.com>
2021-03-03 19:37:19 +0300

    Bir şeyler yaptım :')
    Assetleri yaptım ve gruba bir kısmını attım. Unity'e bütün assetleri ekledim ve açıklama yazdırdım konsola. Projeyi Cemal Can ile yaptık, ben ona buradaki assetleri verdim o da oyunlaştırma kısmını yaptı, onunla grup olduk projemiz birlikte. İyi günler :)

---
+++ Assets/Aciklama.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aciklama : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Cemal ile oyunu yaptık. Kod ve Unity kısmında sadece bunu yapabiliyorum, onun dışında gruba asset dağıttım (Pixel Studion ve Blender kullanarak). Buraya sadece oluşturduğum assetleri ekledim (Scenes dosyasında). Projem Cemal ile oluşturduğumuz oyun, iyi günler dilerim :)");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
